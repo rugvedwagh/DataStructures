2cd6d534cc0741e1b8f1b79fad25069ba31bc8c3
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-09-23 12:13:08 +0530

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,56 +1,41 @@
 #include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Function to partition the array and return the pivot index
-int partition(vector<int>& arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1; // Index of smaller element
-
-    for (int j = low; j <= high - 1; j++) {
-        // If the current element is smaller than or equal to the pivot
-        if (arr[j] <= pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
+int partition(vector<int> &ar, int l, int r){
+    
+  int p = ar[r];
+  
+  int i = l-1;
+  
+  for(int j=l; j<r;j++){
+    if(ar[j]<p){
+      i++;
+      swap(ar[j],ar[i]);
     }
-
-    // Swap the pivot element with the element at index i+1 (position of the pivot in the sorted array)
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
+  }
+  
+  swap(ar[i+1],ar[r]);
+  return i+1;
 }
 
-// Quicksort function
-void quickSort(vector<int>& arr, int low, int high) {
-    if (low < high) {
-        // Find pivot element such that
-        // element smaller than pivot are on the left
-        // element greater than pivot are on the right
-        int pivotIndex = partition(arr, low, high);
-
-        // Recursively sort elements before and after the pivot
-        quickSort(arr, low, pivotIndex - 1);
-        quickSort(arr, pivotIndex + 1, high);
-    }
+void quicksort(vector<int> &ar, int l , int r){
+  if(l<r){
+      
+    int p = partition(ar,l,r);
+    
+    quicksort(ar,l,p-1);
+    quicksort(ar,p+1,r);
+  }
 }
 
-int main() {
-    vector<int> arr = {12, 11, 13, 5, 6, 7};
-
-    cout << "Original array: ";
-    for (int num : arr) {
-        cout << num << " ";
+int main() 
+{
+    vector<int> ar = {5,4,3,2,1};
+    quicksort(ar,0,ar.size()-1);
+    
+    for(int i : ar){
+      cout<<i<<" ";
     }
-    cout << endl;
-
-    int n = arr.size();
-    quickSort(arr, 0, n - 1);
-
-    cout << "Sorted array: ";
-    for (int num : arr) {
-        cout << num << " ";
-    }
-    cout << endl;
-
     return 0;
 }
