81ca97c3db96e843b3415c4e1b39995cbeeab377
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2024-10-25 10:12:35 +0530

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,70 +1,3 @@
-class Node {
-private:
-    Node* links[26];
-    bool flag;
-
-public:
-    Node() {
-        this->flag = false;
-        for (int i = 0; i < 26; i++) {
-            links[i] = NULL;
-        }
-    }
-
-    Node* get(char c) { return links[c-'a']; }
-
-    void put(char c, Node* temp) { links[c - 'a'] = temp; }
-
-    bool containsKey(char c) { return links[c - 'a'] != NULL; }
-
-    void setEnd() { flag = true; }
-
-    bool isEnd() { return flag; }
-};
-
-class Trie {
-private:
-    Node* root;
-
-public:
-    Trie() { root = new Node(); }
-
-    void insert(string word) {
-        Node* temp = root;
-        for (char c : word) {
-            if (!temp->containsKey(c)) {
-                temp->put(c, new Node());
-            }
-            temp = temp->get(c);
-        }
-        temp->setEnd();
-    }
-
-    bool search(string word) {
-        Node* temp = root;
-        for (char c : word) {
-            if (!temp->containsKey(c)) {
-                return false;
-            }
-            temp = temp->get(c);
-        }
-
-        return temp->isEnd();
-    }
-
-    bool startsWith(string prefix) {
-        Node* temp = root;
-
-        for (char c : prefix) {
-            if (!temp->containsKey(c)) {
-                return false;
-            }
-            temp = temp->get(c);
-        }
-
-        return true;
-    }
-};
 
 /**
  * Your Trie object will be instantiated and called as such:
@@ -73,3 +6,62 @@ public:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+class Node {
+public:
+    Node* child[26];
+    bool end;
+
+    Node() {
+        this->end = false;
+        for (int i = 0; i < 26; i++) {
+            child[i] = NULL;
+        }
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() { 
+        root = new Node(); 
+    }
+
+    void insert(string word) {
+        Node* temp = root;
+
+        for (auto i : word) {
+            if (temp->child[i - 'a'] == NULL) {
+                temp->child[i - 'a'] = new Node();
+            }
+            temp = temp->child[i - 'a'];
+        }
+        temp->end = true;
+    }
+
+    bool search(string word) {
+        Node* temp = root;
+
+        for (auto i : word) {
+            if (temp->child[i - 'a'] == NULL) {
+                return false;
+            }
+            temp = temp->child[i - 'a'];
+        }
+
+        return temp->end;
+    }
+
+    bool startsWith(string prefix) {
+        Node* temp = root;
+        for (auto i : prefix) {
+            if (temp->child[i - 'a'] == NULL) {
+                return false;
+            }
+            temp = temp->child[i - 'a'];
+        }
+
+        return true;
+    }
+};
