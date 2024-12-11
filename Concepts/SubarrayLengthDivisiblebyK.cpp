6b8d2213081908c52f375d7948504540dd8a1b8b
Rugved Wagh <89897353+rugvedwagh@users.noreply.github.com>
2024-12-11 15:02:35 +0530

    Update SubarrayLengthDivisiblebyK.cpp

--- Concepts/SubarrayLengthDivisiblebyK.cpp
+++ Concepts/SubarrayLengthDivisiblebyK.cpp
@@ -26,22 +26,10 @@ public:
 };
 
 /*
-This is an application of the Pigeonhole Principle. The idea is that since there are n possible remainders when an integer is divided by n
-that at least two of the n+1 integers in the set {x0,x1,…,xn} must have the same remainder when divided by n.
-If they have the same remainder when divided by n, their difference is divisible by n.
+In an array if we find two indexes which on divided by an integer (k), give the same remainder then,
+the distance between those two indexs is completely divisible by (k)."
 
-The possible remainders when an integer is divided by n are 0,1,2,…,n−1.
-If you have n integers, then it is possible for each of them to have a different remainder when divided by n However, if you have n+1
-integers, at least two of them must have the same remainder when divided by n, Hence, in the set {x0,x1,…,xn}
-, there are two numbers xi and xj with i≠j, that have the same remainder when they are divided by n.
-Thus, there exist k,m,r∈N, with 0≤r≤n−1, such that
+Suppose for eg, i%k == 1 and j%k == 1, then this implies that (j-i) % k == 0.
 
-xixj=kn+r=mn+r
-
-If we take their difference, we obtain
-
-xi−xj=(kn+r)−(mn+r)=kn−mn=(k−m)n
-
-Therefore, xi−xj is divisible by n.
 https://leetcode.com/problems/maximum-subarray-sum-with-length-divisible-by-k/
 */
